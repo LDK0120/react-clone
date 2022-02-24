224630d209ea02f18b887fe91998e4424f78f3d0
Laura Kim <Doyeunkim120@gmail.com>
2022-02-24 04:02:46 -0800

    search section onFocus and onBlur added

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 
 export default function Search() {
-    
+
+    const [border, setBorder] = useState("")
+
+    function handleFocus() {
+        setBorder("color-border")
+    }
+
+    function removeFocus() {
+            setBorder("")
+    }
+
     return (
         <div className="Search">
-            <div className="search-box">
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="search-section-icon" />
+            <div className="search-box" id={border}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="search-section-icon" />  
             <form>
-                <input type="search" placeholder="Search Newsroom" required />
+                <input type="search" placeholder="Search Newsroom" required onFocus={handleFocus} onBlur={removeFocus} />
             </form>
             </div>
           <p className="item search-category">CATEGORIES</p><FontAwesomeIcon icon={faAngleDown} />
